c359df77b464b3e0fafff01f82e2dfd2493a466e
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2024-05-12 19:12:49 +0530

    Create Template for Frontend and Create Frontend Backend Integration

--- app.vue
+++ app.vue
@@ -1,7 +1,41 @@
 <template>
   <div>
-    <h1>Lasitha Hasaranga</h1>
-    <h2>University Of Moratuwa</h2>
-    <h3>IT Faculty</h3>
+    <h1>{{ name }}</h1>
+    <h2>{{ Title1 }}</h2>
+    <ul>
+      <li v-for="Project in Projects" :key="Project.name">
+        <h2>{{ Project.name }}</h2>
+        <p>{{ Project.description }}</p>
+      </li>
+    </ul>
+    <h2>{{ Title2 }}</h2>
+    <ul>
+      <li v-for="Blog in Blogs" :key="Blog.Title">
+        <h2>{{ Blog.Title }}</h2>
+        <p>{{ Blog.Content }}</p>
+        <p>{{ Blog.Date }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const name = "Lasitha Hasaranga";
+const Title1 = "Projects";
+// const Projects = [
+//   {
+//     name : 'L1 Project',
+//     description : 'Object Identification Toy for Childeren',
+//   },
+//   {
+//     name : 'L2 Project',
+//     description : 'LMS System for Students in High School',
+//   },
+// ];
+const {data:Projects, pending1, error1} = useFetch('http://localhost:5000/Projects');
+const Title2 = "Blogs";
+const {data:Blogs, pending2, error2} = useFetch('http://localhost:5000/blogs');
+</script>
+
+<style>
+</style>
