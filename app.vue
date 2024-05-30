c280dc1fc40f92b5599faf7c63d65051e888353c
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2024-05-30 23:11:02 +0530

    Update and Delete a Project using a Form

--- app.vue
+++ app.vue
@@ -6,6 +6,7 @@
       <li v-for="Project in Projects" :key="Project.name">
         <h2>{{ Project.name }}</h2>
         <p>{{ Project.description }}</p>
+        <p><b>ID:</b>{{ Project._id }}</p>
       </li>
     </ul>
     <h2>{{ Title2 }}</h2>
@@ -36,6 +37,44 @@
       </table>
     </form>
   </div>
+  <div>
+    <h1>Update a Project</h1>
+    <form @submit.prevent="changeProject">
+      <table>
+        <tr>
+          <td><label for="id">Project ID:</label></td>
+          <td><input type="text" v-model="updateProject.id" id="id" required></td>
+        </tr>
+        <tr>
+          <td><label for="name">Project Name:</label></td>
+          <td><input type="text" v-model="updateProject.name" id="name" required></td>
+        </tr>
+        <tr>
+          <td><label for="description">Project Description:</label></td>
+          <td><textarea id="description" v-model="updateProject.description" required></textarea></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><button type="submit">Update Project</button></td>
+        </tr>
+      </table>
+    </form>
+  </div>
+  <div>
+    <h1>Delete a Project</h1>
+    <form @submit.prevent="removeProject">
+      <table>
+        <tr>
+          <td><label for="id">Project ID:</label></td>
+          <td><input type="text" v-model="deleteProject.id" id="id" required></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><button type="submit">Delete Project</button></td>
+        </tr>
+      </table>
+    </form>
+  </div>
 </template>
 
 <script setup>
@@ -55,15 +94,18 @@ const {data:Projects, pending1, error1} = useFetch('http://localhost:5000/Projec
 const Title2 = "Blogs";
 const {data:Blogs, pending2, error2} = useFetch('http://localhost:5000/blogs');
 
+//Create a new project
 const newProject = ref({
   name: '', 
   description: '',
 });
 
+//Function to create a new project
 const createProject = async () => {
   console.log(newProject.value);
 
   try{
+
     const response = await fetch('http://localhost:5000/Projects', {
       method: 'POST',
       headers: {
@@ -76,12 +118,88 @@ const createProject = async () => {
       throw new Error('Failed to create a new project');
     }
 
+    //Update the backend with new project
     const result = await response.json();
     Projects.value.push(result);//Add new project to the list
 
     //clear the form
     newProject.value.name = '';
     newProject.value.description = '';
+  }catch(error){
+    console.error(error);
+  }
+};
+
+//Update a project
+const updateProject = ref({
+  id: '',
+  name: '',
+  description: '',
+});
+
+//Function to Update the project using project id
+const changeProject = async () => {
+  console.log(updateProject.value);
+
+  try{
+    const response = await fetch(`http://localhost:5000/Projects/${updateProject.value.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateProject.value),
+    });
+
+    if(!response.ok){
+      throw new Error('Failed to update the project');
+    }
+
+    //Update the backend with updated project 
+    const result = await response.json();
+
+    //replace the element within project list by checking the _id
+    const index = Projects.value.findIndex((project) => project._id === updateProject.value.id);
+    if(index !== -1) {
+      Projects.value[index] = result;
+    }
+    
+    //clear the form
+    updateProject.value.id = '';
+    updateProject.value.name = '';
+    updateProject.value.description = '';
+  }catch(error){
+    console.error(error);
+  }
+};
+
+//Delete a project
+const deleteProject = ref({
+  id: '',
+});
+
+//Function to delete the project using project id
+const removeProject = async () => {
+  console.log(deleteProject.value);
+
+  try{
+    const response = await fetch(`http://localhost:5000/Projects/${deleteProject.value.id}`, {
+      method: 'DELETE',
+    });
+
+    if(!response.ok){
+      throw new Error('Failed to delete the project');
+    }
+
+    //update the backend with deleted project
+    const result = await response.json();
+    //Remove the project from the list
+    const index = Projects.value.findIndex((project) => project._id === deleteProject.value.id);
+    if(index !== -1) {
+      Projects.value.splice(index, 1);
+    }
+    
+    //clear the form
+    deleteProject.value.id = '';
   }catch(error){
     console.error(error);
   }
